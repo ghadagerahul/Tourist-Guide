38f1d1434d92155ea9339615cab93f74db8008c6
Ghadage Rahul <inb322rahgha@ext.crifnet.com>
2025-02-22 17:26:46 +0530

    reactive for to template based form

--- Tourist-Guide/Tourist-Guide-UI/src/app/components/registeration-page/registeration-page.component.ts
+++ Tourist-Guide/Tourist-Guide-UI/src/app/components/registeration-page/registeration-page.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AppService } from '../../services/app.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-registeration-page',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, FormsModule, CommonModule],
   templateUrl: './registeration-page.component.html',
   styleUrl: './registeration-page.component.css'
 })
@@ -14,9 +15,13 @@ export class RegisterationPageComponent implements OnInit {
 
   constructor(private appservice: AppService,
     private fb: FormBuilder
-  ) {
-
+  ) { 
+    
   }
+
+
+
+
   ngOnInit(): void {
 
     this.registrationForm = this.fb.group({
@@ -31,25 +36,24 @@ export class RegisterationPageComponent implements OnInit {
 
   }
 
-
-
-
-  // registrationForm: any = new FormGroup({
-  //   firstName: new FormControl(''),
-  //   lastName: new FormControl(''),
-  //   email: new FormControl(''),
-  //   mobileNo: new FormControl(''),
-  //   userName: new FormControl(''),
-  //   password: new FormControl('')
-  // });
-
-
-
-
+  registrationForm1: any = ({
+    firstName: [''],
+    lastName: [''],
+    email: [''],
+    mobileNo: [''],
+    userName: [''],
+    password: ['']
+  })
 
   onSubmitData() {
+    
+    this.registrationForm.value.firstName = this.registrationForm1.firstName;
+    this.registrationForm.value.lastName = this.registrationForm1.lastName;
+    this.registrationForm.value.email = this.registrationForm1.email;
+    this.registrationForm.value.mobileNo = this.registrationForm1.mobileNo;
+    this.registrationForm.value.userName = this.registrationForm1.userName;
+    this.registrationForm.value.password = this.registrationForm1.password;
 
-    this.registrationForm.value;
     console.log(this.registrationForm.value)
 
     if (this.registrationForm.valid) {
